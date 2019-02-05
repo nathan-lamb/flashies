4dfc23ab04c36d4cb0718adcbb4dcb3236af9065
Nathan Lamb <nathanlamb022@gmail.com>
2019-02-05 08:47:01 +0100

    Remove boilerplate

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+// const bodyParser = require('body-parser')
+// const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/ping', function (req, res) {
     return res.send('pong pong');
-});
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
