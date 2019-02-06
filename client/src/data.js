c9bba6844e54a28dbb8d5fa1d3a6fa6e5eb25f3d
Nathan Lamb <nathanlamb022@gmail.com>
2019-02-06 11:22:55 +0100

    Add dummy data

---
+++ client/src/data.js
@@ -0,0 +1,10 @@
+const cards = [
+    { id: 1, front: 'blau', back: 'blue' },
+    { id: 2, front: 'die Katze', back: 'cat' },
+    { id: 3, front: 'vermeiden', back: 'to avoid' },
+    { id: 4, front: 'die Kinder', back: 'children' },
+    { id: 5, front: 'sich verabschieden', back: 'to say goodbye' },
+    { id: 6, front: 'für', back: 'for' }
+]
+
+export default cards
