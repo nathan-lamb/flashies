e355fb3dba0ab00fa5a8b3e83534ea6af71fe2ef
Nathan Lamb <nathanlamb022@gmail.com>
2019-02-06 11:22:29 +0100

    Add API to get all cards from db

--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ const express = require('express');
 // const path = require('path');
 const app = express();
 
-app.get('/ping', function (req, res) {
-    return res.send('pong pong');
+const knexfile = require('./knexfile')
+const knex = require('knex')(knexfile.development)
+
+app.get('/cards', function (req, res) {
+    knex.select().from('default_deck').then(cards => {
+        res.send(cards);
+    })
 });
 
 app.listen(process.env.PORT || 8080);
