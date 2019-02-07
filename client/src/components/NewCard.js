8e978c879b6ba0716bc3fd72d8619fb32b64defa
Nathan Lamb <nathanlamb022@gmail.com>
2019-02-07 14:43:39 +0100

    Add New Card form

--- client/src/components/NewCard.js
+++ client/src/components/NewCard.js
@@ -1,25 +1,16 @@
 import React from 'react'
 
-import { Button, Header, Input, Segment } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
 
 import '../App.css'
 
 const NewCard = () => {
     return (
-        <div>
-            <Header as='h3' content='Add New Card' textAlign='center' />
-            <Segment>
-                Front
-                <Input />
-            </Segment>
-            <Segment>
-                Back
-                <Input />
-            </Segment>
-            <Button.Group floated='right'>
-                <Button icon='plus' />
-            </Button.Group>
-        </div>
+        <Form>
+            <Form.Input label='Front' />
+            <Form.Input label='Back' />
+            <Form.Button floated='right' icon='plus' />
+        </Form>
     )
 }
 
